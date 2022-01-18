37313a14af773dfe216ffa91ab1e4e1359a2d08b
Stephen Tridgell <stephen.tridgell@cruxml.com>
2022-01-18 13:04:08 +1100

    Add MANIFEST.in to remove the BUILD files.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 DESCRIPTION = "A standalone package to do the vitis quantization step."
 
 REQUIRED_PACKAGES = [
@@ -19,7 +19,7 @@ setup(
     install_requires=REQUIRED_PACKAGES,
     long_description=open("README.md").read(),
     download_url=f"https://github.com/CruxML/vitis-quantizer/archive/refs/tags/{VERSION}.tar.gz",
-    packages=find_packages(exclude=["*_test.py"]),
+    packages=find_packages(),
     license="Apache 2.0",
     include_package_data=True,
     keywords="vitis quantize machine learning xilinx",
